a8c76d0ec01cc34df3686a7a594a51a5dc071f22
rubayetseason <season.ss2001@gmail.com>
2022-08-22 16:13:50 +0600

    calclulation button partially worked

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
-function addToList (element){
-// here we get the player names when the button is clicked 
-const playerName = element.parentNode.parentNode.children[0].innerText;
-// add li to the list 
-const placeList = document.getElementById('place-list');
+function addToList(element) {
+    // button disabled after click
+    element.disabled = true;
+    // here we get the player names when the button is clicked 
+    const playerName = element.parentNode.parentNode.children[0].innerText;
+    // add li to the list 
+    const placeList = document.getElementById('place-list');
 
-if (placeList.childElementCount > 4){
-    // alert message here 
-    alert('cannot add more than 5 players')
+    if (placeList.childElementCount > 4) {
+        // alert message here 
+        alert('cannot add more than 5 players')
+    }
+    else {
+        const li = document.createElement('li')
+        li.innerText = playerName;
+        placeList.appendChild(li);
+        // done adding li to the list 
+        const playerNumber = placeList.childElementCount;
+        
+        return playerNumber;
+    }
 }
-else{
-    const li =  document.createElement('li')
-    li.innerText = playerName;
-    placeList.appendChild(li);
-    console.log(placeList.childElementCount);
-    // done adding li to the list 
-}
-}
+
+
+
+document.getElementById('calculate-btn').addEventListener('click', function(){
+// get the input field value 
+    const playerValue = document.getElementById('per-player-value').value;
+// gotten the input value 
+
+})
