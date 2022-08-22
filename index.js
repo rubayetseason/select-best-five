8e9565d7919cb6df60e1b5ed8b0849be33a26601
rubayetseason <season.ss2001@gmail.com>
2022-08-22 15:35:39 +0600

    player count and alert message done

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 function addToList (element){
 // here we get the player names when the button is clicked 
 const playerName = element.parentNode.parentNode.children[0].innerText;
+// add li to the list 
+const placeList = document.getElementById('place-list');
+
+if (placeList.childElementCount > 4){
+    // alert message here 
+    alert('cannot add more than 5 players')
+}
+else{
+    const li =  document.createElement('li')
+    li.innerText = playerName;
+    placeList.appendChild(li);
+    console.log(placeList.childElementCount);
+    // done adding li to the list 
+}
 }
